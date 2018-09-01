7d8de1b4f3551a4a711e02c82acf7a416bb4d1c1
Joop Kiefte <ikojba@gmail.com>
2018-09-01 02:20:02 +0200

    A bit more useful code

--- logic.go
+++ logic.go
@@ -2,9 +2,26 @@
 
 package main
 
-import "honnef.co/go/js/dom"
+import (
+	"honnef.co/go/js/dom"
+	"fmt"
+)
+
+func nav(n map[string]string) {
+	a := func(t, h string) string {
+		return fmt.Sprintf(`<a class="mdl-navigation__link" href="%s">%s</a>`, h, t)
+	}
+	el := dom.GetWindow().Document().QuerySelector("nav")
+	var menu string
+	for t, h := range n {
+		menu += a(t, h)
+	}
+	el.SetInnerHTML(menu)
+}
 
 func main() {
-	el := dom.GetWindow().Document().QuerySelector("nav")
-	el.SetInnerHTML(`<a class="mdl-navigation__link" href="javascript:alert('Yay!');">Javascript is activated!`)
+	menu := map[string]string {
+		"en": "#en",
+	}
+	nav(menu)
 }
